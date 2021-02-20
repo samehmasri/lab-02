a523fe2f8c84717a450cf14c16a31cd9acf55a75
samehmasri <sameh.s.almasri@gmail.com>
2021-02-20 19:46:22 +0200

    lab2

--- js/app.js
+++ js/app.js
@@ -1,55 +1,82 @@
+'use strict';
 
+let keyArray = []; // array of keywords\
 
-let AllData = [];
-$.ajax("../data/page-1.json").then(data => {
-    data.forEach(element => {
+// take the data and loop it and make object for everyone of them
+$.ajax('data/page-1.json')
+    .then(data => {
+        data.forEach((item) => {
 
+            let newHorn = new Horn(item);
 
-        let Data = new animal(element);
-        AllData.push(element);
+            newHorn.render();
+        })
+        renderHorn();
+    })
+// constructer function takes the data from page-1.json and creates it as an object
+function Horn(value) {
+    this.title = value.title;
+    this.description = value.description;
+    this.keyword = value.keyword;
+    this.image_url = value.image_url;
+}
+// prototype renders the data and append it to html
+Horn.prototype.render = function () {
 
+    let hornClone = $('.photo-template').clone();
+    hornClone.removeClass('photo-template');
+    hornClone.addClass(this.keyword);//class for each element
+    hornClone.find('.h2Text').text(this.title);
+    // find the img atrr and give it css codes
+    hornClone.find('img').attr({
+        src: this.image_url,
+        // width: "200px",
+        alt: this.title
+    }
+    )
+    hornClone.find('p').text(this.description);
+    $('main').append(hornClone);
 
-        Data.render();
+    if (!(keyArray.includes(this.keyword))) {//for the repetation tekrrar
+        keyArray.push(this.keyword);
+    }
+}
+// console.log(keyArray);
+
+function renderHorn() {
+    keyArray.forEach(element => {
+
+        $('select').append(`<option value="${element}">${element}</option>`);
     });
-    $(".photo-template").first().hide();
-})
-function animal(a) {
-    this.image = a.image_url;
-    this.title = a.title;
-    this.description = a.description;
-    this.horns = a.horns;
-    this.keyword = a.keyword;
 }
-animal.prototype.render = function () {
 
-    let template = $(".photo-template").first().clone();
-    template.find("h2").text(this.title);
-    template.find("img").attr("src", this.image);
-    template.find("p").text(this.description);
-    $("main").append(template)
 
-}
-$("#selection").change(function (e) {
-    e.preventDefault();
-    $("main").html("");
-    let divTemp = $("main").append("<div></div>");
-    $("div").addClass('photo-template');
-    AllData.forEach(element => {
-        if (element.keyword == $("#selection").val()) {
-            Render(element);
+$('select').on('change', function () {
+    console.log($(this).val());
+    for (let i = 0; i < keyArray.length; i++) {
+        $(`.${keyArray[i]}`).css('display', '');
+        //delete then render again
+    }
 
+
+    for (let i = 0; i < keyArray.length; i++) {
+        if ("default" == $(this).val()) {
+            $(`.${keyArray[i]}`).toggle();
         }
 
-    });
-    $(".photo-template").first().hide();
-});
-let Render = render = d => {
-    let template = $(".photo-template").first().clone();
-    template.append("<h2></h2>")
-    template.append("<img></img>")
-    template.append("<p></p>")
-    template.find("h2").text(d.title);
-    template.find("img").attr("src", d.image_url);
-    template.find("p").text(d.description);
-    $("main").append(template)
-}
+    }
+
+    for (let i = 0; i < keyArray.length; i++) {
+        if (keyArray[i] != $(this).val()) {
+            $(`.${keyArray[i]}`).toggle();
+        }
+    }
+    // trying to make the code as Razan said but it didn't work
+    // $('div').hide();
+    // let selectdOption = $(this).val();
+    // $(selectdOption).fadeIn(800);
+    // $('.${selectdOption}').fadeIn(800);
+    // console.log(this.val);
+
+
+})
